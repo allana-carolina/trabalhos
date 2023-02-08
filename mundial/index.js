1ee8bbebc353e3dd7fb359050ba5ce025b80706e
ismael <ismaelwadejr07@gmail.com>
2023-02-08 11:49:26 -0300

    up

---
+++ mundial/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const server = express();
+server.use(express.json());
+
+const projects = [];
+
+server.listen(4000);
+
+
+/**
+ * Request body: id, title
+ * Cadastra um novo projeto
+ */
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+server.get("/", (req, res) => {
+  res.send(projects)
+  });
